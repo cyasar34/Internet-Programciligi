2f2dc5351b98311ea6ce9c7e29faca5df8fdc18a
Lect. Çisem YAŞAR <97396158+cyasar34@users.noreply.github.com>
2024-10-18 14:51:13 +0300

    Add files via upload

---
+++ hafta3.php
@@ -0,0 +1,44 @@
+<?php
+//isset,unset kullanımı
+/*echo "isset($AdiSoyadi)"."<br>";
+$AdiSoyadi="Çisem Yaşar";
+echo "isset($AdiSoyadi)"."<br>";
+
+$degisken="Gelişim Üniversitesi";
+echo $degisken."<br>";
+unset($degisken);
+echo $degisken;*/
+
+//Doğum yılı hesaplama
+/*$yil=2024;
+$dogum_yili=1987;
+$yas=$yil-$dogum_yili;
+echo "Benim Yaşım ".$yas;*/
+
+//Basit hesap makinesi
+/*echo "<h3>Basit Hesap Makinesi</h3>"."<br>";
+$sayi1=35;
+$sayi2=5;
+$topla=$sayi1+$sayi2;
+$cikar=$sayi1-$sayi2;
+$carp=$sayi1*$sayi2;
+$bol=$sayi1/$sayi2;
+$mod=$sayi1%$sayi2;
+$us=$sayi1**$sayi2;
+echo "1.sayının değeri: ".$sayi1."<br>".
+    "2.sayının değeri: ".$sayi2."<br>".
+    "Toplama işleminin sonucu: ".$topla."<br>".
+    "Çıkarma işleminin sonucu: ".$cikar."<br>".
+    "Çarpma işleminin sonucu: ".$carp."<br>".
+    "Bölme işleminin sonucu: ".$bol."<br>".
+    "Mod alma işleminin sonucu: ".$mod."<br>".
+    "Üs alma işleminin sonucu: ".$us;*/
+
+
+//Artırma ve Azaltma Operatorleri Kullanımı
+$sayi=15;
+echo "<u>Yazmış olduğunuz sayının değeri: </u>".$sayi."<br>".
+"Sayının değerini artırma işlemi: ".++$sayi."<br>".
+"Sayının değerini azaltma işlemi: ".--$sayi;
+
+?>
