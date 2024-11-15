16d41a5ce82dfd13841c8d4ff6f587c6711213de
Lect. Çisem YAŞAR <97396158+cyasar34@users.noreply.github.com>
2024-11-15 21:52:54 +0300

    Add files via upload

---
+++ hafta7_for_karesini_alma.php
@@ -0,0 +1,11 @@
+<?php
+
+for ($i=1;$i<=10;$i++)
+{
+    //echo $i." sayısının karesi: ".$i*$i."<br>";
+    //echo $i." sayısının karesi: ".($i**2)."<br>";
+    echo $i." sayısının karesi: ".pow($i,2)."<br>";
+}
+?>
+
+
