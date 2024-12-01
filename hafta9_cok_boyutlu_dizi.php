ea56fcb62c390fc87725335c4381782148051b2f
Lect. Çisem YAŞAR <97396158+cyasar34@users.noreply.github.com>
2024-12-01 22:45:45 +0300

    Add files via upload

---
+++ hafta9_cok_boyutlu_dizi.php
@@ -0,0 +1,9 @@
+<?php
+$ogrenci=array();
+$ogrenci[0]=array("Çisem","Balıkesir");
+$ogrenci[1]=array("Ahmet","Bursa");
+$ogrenci[2]=array("Banu","Van");
+echo "<pre>";
+print_r($ogrenci);
+echo "</pre>";
+?>
